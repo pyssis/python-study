365064d7dd0e5fe09197ede7a1e685f685e9a08e
Valeria Fedorishchenko <pyssis@pyssis.com>
2024-02-20 19:13:55 +0200

    Add files via upload

--- cat1.py
+++ cat1.py
@@ -1,16 +1,12 @@
-a1 = int(input("Length in centimeters: "))  # in cm: 46
-b1 = int(input("Height in centimeters: "))  # in cm: 31
-c1 = int(input("Width in centimeters: "))  # in cm: 34
+a = int(input("Length in centimeters: "))  # in cm: 46
+b = int(input("Height in centimeters: "))  # in cm: 31
+c = int(input("Width in centimeters: "))  # in cm: 34
 m = int(input("Mass in kilogram: "))  # in kg: 10
-a = a1/100
-b = b1/100
-c = c1/100
 V = a*b*c
-V1 = V*100
-print("Volume of a cat:", round(V1), "cm^3.")
-p1 = m/V1
-p = p1*1000
-print("Density of a cat: ", round(p1), " g/cm^3 (", round(p), " kg/m^3)", sep="")
+print("Volume of a cat:", V, "cm^3.")
+M = m*1000  #kg in g
+p = M/V
+print("Density of a cat: ", p, " g/cm^3.")
 
 # p=m/V, where m - mass, V - volume
 # V=abc, where a, b, c - cat sides
