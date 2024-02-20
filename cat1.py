1c4b8932818463eec1cdd216b9c52a17210f186f
Valeria Fedorishchenko <pyssis@pyssis.com>
2024-02-20 18:49:36 +0200

    Add files via upload

---
+++ cat1.py
@@ -0,0 +1,16 @@
+a1 = int(input("Length in centimeters: "))  # in cm: 46
+b1 = int(input("Height in centimeters: "))  # in cm: 31
+c1 = int(input("Width in centimeters: "))  # in cm: 34
+m = int(input("Mass in kilogram: "))  # in kg: 10
+a = a1/100
+b = b1/100
+c = c1/100
+V = a*b*c
+V1 = V*100
+print("Volume of a cat:", round(V1), "cm^3.")
+p1 = m/V1
+p = p1*1000
+print("Density of a cat: ", round(p1), " g/cm^3 (", round(p), " kg/m^3)", sep="")
+
+# p=m/V, where m - mass, V - volume
+# V=abc, where a, b, c - cat sides
--- cat2.py
+++ cat2.py
@@ -1,13 +1,11 @@
-a1 = int(input("Length in centimeters: "))  # in cm: 46
-b1 = int(input("Height in centimeters: "))  # in cm: 31
-c1 = int(input("Width in centimeters: "))  # in cm: 34
-a = a1/100
-b = b1/100
-c = c1/100
-V1 = a*b*c
-V = V1*100
-print("Volume of a cat:", round(V), "m^3.")
-UranP = 19.05  # g/cm^3
-p = UranP*1000
+a = int(input("Length in centimeters: "))  # in cm: 46
+b = int(input("Height in centimeters: "))  # in cm: 31
+c = int(input("Width in centimeters: "))  # in cm: 34
+V = a*b*c
+print("Volume of a cat:", round(V), "cm^3.")
+p = 19.05  # g/cm^3
 m = V*p
-print("Mass of a cat:", round(m), "kg.")
+M = m*0.001
+print("Mass of a cat:", round(M), "kg.")
+
+# m=pV
